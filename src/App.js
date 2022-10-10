dde2291394470b42cf51712f673cda660fad07e0
Spencer Michalski <semski13@gmail.com>
2022-10-10 15:27:48 -0400

    add random keys and tempo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
 import { useState } from "react";
 
-function App() {
-  const [randomizeTempo, setRandomizeTempo] = useState(false);
-  const [randomizeKey, setRandomizeKey] = useState(false);
+const KEYS = [
+  { major: "A", minor: "F#m" },
+  { major: "E", minor: "C#m" },
+  { major: "B", minor: "G#m" },
+  { major: "F#", minor: "D#m" },
+  { major: "Db", minor: "Bbm" },
+  { major: "Ab", minor: "Fm" },
+  { major: "Eb", minor: "Cm" },
+  { major: "Bb", minor: "Gm" },
+  { major: "F", minor: "Dm" },
+  { major: "C", minor: "Am" },
+  { major: "G", minor: "Em" },
+  { major: "D", minor: "Bm" },
+];
+const NO_KEY = null;
+const NO_TEMPO = null;
 
-  const areAnyCheckboxesSelected = randomizeKey || randomizeTempo;
+function App() {
+  const [isTempoChecked, setIsTempoChecked] = useState(false);
+  const [isKeyChecked, setIsKeyChecked] = useState(false);
+
+  const [tempo, setTempo] = useState(NO_TEMPO);
+  const [key, setKey] = useState(NO_KEY);
+
+  const toggleIsTempoChecked = () => setIsTempoChecked(!isTempoChecked);
+  const toggleKeyChecked = () => setIsKeyChecked(!isKeyChecked);
+
+  const isFormValid = isKeyChecked || isTempoChecked;
+
+  const getRandomIntBetween = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  const getRandomTempo = () => {
+    const MIN_TEMPO = 40;
+    const MAX_TEMPO = 240;
+    return getRandomIntBetween(MIN_TEMPO, MAX_TEMPO);
+  };
+
+  const getRandomKey = () => {
+    const isMinor = getRandomIntBetween(0, 1) == 0;
+    const index = getRandomIntBetween(0, 11);
+    return isMinor ? KEYS[index].minor : KEYS[index].major;
+  };
+
+  const inspire = () => {
+    setTempo(isTempoChecked ? getRandomTempo() : NO_TEMPO);
+    setKey(isKeyChecked ? getRandomKey() : NO_KEY);
+  };
 
   return (
     <div className="flex flex-col items-center">
@@ -34,32 +78,27 @@ function App() {
       <br />
       <h2 className="text-xl">Select your constraint(s):</h2>
       <div>
-        <div>
+        <div className="space-x-1">
+          <input id="tempo" type="checkbox" onClick={toggleIsTempoChecked} />
           <label for="tempo">Tempo</label>
-          <input
-            id="tempo"
-            type="checkbox"
-            onClick={() => setRandomizeTempo(!randomizeTempo)}
-          />
+          <input id="key" type="checkbox" onClick={toggleKeyChecked} />
           <label for="key">Key</label>
-          <input
-            id="key"
-            type="checkbox"
-            onClick={() => setRandomizeKey(!randomizeKey)}
-          />
         </div>
         <button
-          disabled={!areAnyCheckboxesSelected}
+          disabled={!isFormValid}
           className="text-3xl shadow-lg rounded bg-purple-700 text-white enabled:hover:bg-purple-500 p-3 disabled:bg-slate-400"
+          onClick={() => inspire()}
         >
           Inspire Me
         </button>
       </div>
-      {!areAnyCheckboxesSelected && (
+      {!isFormValid && (
         <p className="text-sm text-red-700">
           Please select at least one constraint to continue!
         </p>
       )}
+      {tempo && <p>Randomly Chosen Tempo: {tempo}</p>}
+      {key && <p>Randomly Chosen Key: {key}</p>}
       {/* Consider adding legal disclaimer (fine print at the bottom) that we don't own any rights to the songs created */}
     </div>
   );
