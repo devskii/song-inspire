6b3179d765481164f8f9b3db179198b744582272
Spencer Michalski <semski13@gmail.com>
2022-10-10 10:53:48 -0400

    add intro copy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>SongInspire</h1>
+      <p>
+        Welcome to SongInspire! Need help getting inspired in your songwriting?
+        Ever look at the blank piece of paper in front of you and not know where
+        to start? Here at SongInspire, we believe the best way to spark
+        inspiration is to introduce constraints on your song. They will be your
+        guardrails, giving you a space in which to create.
+      </p>
+      <p>
+        Use this app to select any number of constraints. To give yourself a fun
+        challenge, somewhere to start. Then write your song with that constraint
+        applied! For example, you could say, "All right, I'm going to write a
+        song at 120bpm."
+      </p>
+      <p>
+        All selections are random, so there's a ton of variety you can harness
+        in your creative endeavors!
+      </p>
+    </div>
+  );
 }
 
 export default App;
