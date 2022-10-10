0e6e4605ef9c83aa51342112026efe6c328800a9
Spencer Michalski <semski13@gmail.com>
2022-10-10 11:16:36 -0400

    add basic checkboxes and button

--- src/App.js
+++ src/App.js
@@ -3,22 +3,33 @@ function App() {
     <div>
       <h1>SongInspire</h1>
       <p>
-        Welcome to SongInspire! Need help getting inspired in your songwriting?
-        Ever look at the blank piece of paper in front of you and not know where
-        to start? Here at SongInspire, we believe the best way to spark
-        inspiration is to introduce constraints on your song. They will be your
-        guardrails, giving you a space in which to create.
+        Need help getting inspired in your songwriting? Ever look at the blank
+        piece of paper in front of you and not know where to start?
       </p>
       <p>
-        Use this app to select any number of constraints. To give yourself a fun
-        challenge, somewhere to start. Then write your song with that constraint
-        applied! For example, you could say, "All right, I'm going to write a
-        song at 120bpm."
+        Here at SongInspire, we believe the best way to spark inspiration is to
+        introduce constraints on your creative endeavor. You're giving yourself
+        guardrails, carving out a space from which to create.
+      </p>
+      <p>
+        Use this app to select any number of constraints. Give yourself a fun
+        challenge; give yourself somewhere to start. Then write your song with
+        that constraint applied! For example, you could say, "All right, I'm
+        going to write a song at 120bpm."
       </p>
       <p>
         All selections are random, so there's a ton of variety you can harness
         in your creative endeavors!
       </p>
+      <h2>Select your constraint(s):</h2>
+      <div>
+        <label for="tempo">Tempo</label>
+        <input id="tempo" type="checkbox" />
+        <label for="key">Key</label>
+        <input id="key" type="checkbox" />
+      </div>
+      <button>Inspire Me</button>
+      {/* Consider adding legal disclaimer (fine print at the bottom) that we don't own any rights to the songs created */}
     </div>
   );
 }
